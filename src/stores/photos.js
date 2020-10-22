e9c32064e81cad23e4ea45cb1f0baf57ec86a731
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-10-23 00:35:26 +0300

    fixed a one off error

--- src/stores/photos.js
+++ src/stores/photos.js
@@ -1,15 +1,12 @@
 import { default as assets } from "./assets.json";
 
 let index = 0;
-const last = assets.length - 1;
-
 export const photos = (batchSize) => {
   const start = index;
-  const end = Math.min(start + batchSize, last);
-  console.log("start:" + start + " end:" + end);
+  const end = Math.min(start + batchSize, assets.length);
   index += batchSize;
   return new Promise((resolve, reject) => {
-    if (start <= last) {
+    if (start < assets.length) {
       resolve(assets.slice(start, end));
     } else {
       reject();
