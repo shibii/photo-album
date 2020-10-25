08738d92845cad5503b4c1f05a9d2448e215aa68
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-10-25 18:22:53 +0200

    static build

--- _dist_/global.css.proxy.js
+++ _dist_/global.css.proxy.js
@@ -1,5 +1,5 @@
 
-const code = ":root {\r\n  --theme0: #0b0f16;\r\n  --theme1: #161f2e;\r\n  --theme2: #1f2d46;\r\n  --theme3: #2e63be;\r\n}\r\n\r\nhtml {\r\n  box-sizing: border-box;\r\n  height: 100%;\r\n}\r\n\r\n*,\r\n*::before,\r\n*::after {\r\n  box-sizing: inherit;\r\n}\r\n\r\n* {\r\n  padding: 0;\r\n  margin: 0;\r\n}\r\n\r\nbody {\r\n  font-family: \"Commissioner\", sans-serif;\r\n  height: 100%;\r\n  background-color: var(--theme0);\r\n}\r\n\r\nimg {\r\n  display: block;\r\n}\r\n";
+const code = ":root {\r\n  --theme0: #19201f;\r\n  --theme1: #333b3a;\r\n  --theme2: #454b57;\r\n  --theme3: #676f7e;\r\n}\r\n\r\nhtml {\r\n  box-sizing: border-box;\r\n  height: 100%;\r\n}\r\n\r\n*,\r\n*::before,\r\n*::after {\r\n  box-sizing: inherit;\r\n}\r\n\r\n* {\r\n  padding: 0;\r\n  margin: 0;\r\n}\r\n\r\nbody {\r\n  font-family: \"Commissioner\", sans-serif;\r\n  height: 100%;\r\n  background-color: var(--theme0);\r\n}\r\n\r\nimg {\r\n  display: block;\r\n}\r\n";
 
 const styleEl = document.createElement("style");
 const codeEl = document.createTextNode(code);
