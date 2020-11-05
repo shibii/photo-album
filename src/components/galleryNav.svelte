523a976f576fab6b812e24c05852862017bfef46
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-11-05 14:02:38 +0200

    added a component for tag listing

---
+++ src/components/galleryNav.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { photos } from "../stores/photos";
+  import Tag from "./tag.svelte";
+
+  const tags = photos.tags;
+</script>
+
+<style>
+  h5 {
+    margin-bottom: 0.5em;
+  }
+  .tag {
+    margin-right: 2px;
+  }
+</style>
+
+<h5>browse photos by tags:</h5>
+{#each tags as tag (tag)}
+  <span class="tag">
+    <Tag {tag} />
+  </span>
+{/each}
