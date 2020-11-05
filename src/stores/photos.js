d29f89ac278c77d4af10e8a1de5e3c63435522f5
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-11-05 14:01:21 +0200

    added tag functionality to photos store

--- src/stores/photos.js
+++ src/stores/photos.js
@@ -1,7 +1,32 @@
 import { default as assets } from "./assets.json";
+import { parse } from "date-fns";
 
-const all = () => {
-  return assets;
+// convert timestamp strings to actual date objects
+assets.forEach(
+  (asset) =>
+    (asset.timestamp = parse(
+      asset.timestamp,
+      "yyyy:MM:dd HH:mm:ss",
+      new Date()
+    ))
+);
+
+const all = assets;
+
+let tags = assets.map((asset) => asset.tags).flat();
+
+var counts = {};
+tags.forEach((tag) => {
+  counts[tag] = (counts[tag] || 0) + 1;
+});
+tags = [];
+for (const [tag, count] of Object.entries(counts)) {
+  tags.push({ tag, count });
+}
+
+const byTag = (tag, subset) => {
+  let set = subset || assets;
+  return set.filter((photo) => photo.tags.includes(tag));
 };
 
-export const photos = { all };
+export const photos = { all, tags, byTag };
