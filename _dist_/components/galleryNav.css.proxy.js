f1c5b67cc3021236a9518530bd5065c5b8569c3a
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-11-06 00:28:55 +0200

    static build

--- _dist_/components/galleryNav.css.proxy.js
+++ _dist_/components/galleryNav.css.proxy.js
@@ -1,5 +1,5 @@
 
-const code = "h5.svelte-1wqbhv4{margin-bottom:0.1em;margin-top:1em}.tag.svelte-1wqbhv4{margin-right:2px}";
+const code = "h4.svelte-jcdadb{margin-bottom:0.2em;margin-top:1em}div.tag.svelte-jcdadb{display:inline-block;margin-right:2px;margin-bottom:1px}";
 
 const styleEl = document.createElement("style");
 const codeEl = document.createTextNode(code);
--- _dist_/components/tag.css.proxy.js
+++ _dist_/components/tag.css.proxy.js
@@ -1,5 +1,5 @@
 
-const code = "button.svelte-9dpvxy{border-radius:3px;border:1px solid;padding:0.1rem 0.5em;font-weight:bold;font-size:0.7rem;background-color:var(--theme1);color:var(--contrast0)}button.svelte-9dpvxy:hover{background-color:var(--contrast0);color:var(--theme1)}span.svelte-9dpvxy{margin-left:0.5em}";
+const code = "button.svelte-mlynuv{border-radius:3px;border:1px solid;padding:0.1rem 0.5em;font-weight:bold;font-size:0.75rem;background-color:var(--theme1);color:var(--contrast0)}button.svelte-mlynuv:hover{background-color:var(--contrast0);color:var(--theme1)}span.svelte-mlynuv{margin-left:0.5em}";
 
 const styleEl = document.createElement("style");
 const codeEl = document.createTextNode(code);
